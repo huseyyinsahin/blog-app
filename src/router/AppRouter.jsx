57b3a615fe6fdf4163737abf78d1a115afcd58f1
huseyyinsahin <huseysahin19@outlook.com>
2024-10-25 21:18:06 +0300

    React lazy

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,46 +1,63 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Outlet, Route, Routes } from "react-router-dom";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
+import { CircularProgress, Container } from "@mui/material";
 import Navbar from "../components/Navbar";
-import Blogs from "../pages/Blogs";
-import About from "../pages/About";
-import Profile from "../pages/Profile";
-import NewBlog from "../pages/NewBlog";
-import MyBlogs from "../pages/MyBlogs";
 import PrivateRouter from "./PrivateRouter";
 import Footer from "../components/Footer";
+
+import Blogs from "../pages/Blogs";
+import MyBlogs from "../pages/MyBlogs";
 import Detail from "../pages/Detail";
-import NotFound from "../pages/NotFound";
-import Home from "../pages/Home";
+
+const Home = lazy(() => import("../pages/Home"));
+const Login = lazy(() => import("../pages/Login"));
+const Register = lazy(() => import("../pages/Register"));
+const About = lazy(() => import("../pages/About"));
+const Profile = lazy(() => import("../pages/Profile"));
+const NewBlog = lazy(() => import("../pages/NewBlog"));
+const NotFound = lazy(() => import("../pages/NotFound"));
 
 function AppRouter() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          element={
-            <>
-              <Outlet />
-              <Footer />
-            </>
-          }
-        >
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/about" element={<About />} />
-          <Route path="" element={<PrivateRouter />}>
-            <Route path="/newblog" element={<NewBlog />} />
-            <Route path="/myblogs" element={<MyBlogs />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/blogs/detail/:id" element={<Detail />} />
+      <Suspense
+        fallback={
+          <Container
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "200px",
+            }}
+          >
+            <CircularProgress size={100} />
+          </Container>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            element={
+              <>
+                <Outlet />
+                <Footer />
+              </>
+            }
+          >
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/about" element={<About />} />
+            <Route path="" element={<PrivateRouter />}>
+              <Route path="/newblog" element={<NewBlog />} />
+              <Route path="/myblogs" element={<MyBlogs />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/blogs/detail/:id" element={<Detail />} />
+            </Route>
           </Route>
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
